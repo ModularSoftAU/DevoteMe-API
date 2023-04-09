e0857b31521d71aacf17e635e23dd4197a0c6c48
benrobson <shadowolfbusiness@gmail.com>
2023-04-09 23:47:21 +1000

    Push DevoteMe devotion feature from intouchaustralia

---
+++ app.js
@@ -0,0 +1,37 @@
+import fastify from 'fastify';
+import config from "./config.json" assert {type: "json"};
+import packageData from './package.json' assert {type: "json"};
+
+// Site Routes
+import siteRoutes from './routes'
+
+//
+// Application Boot
+//
+const buildApp = async () => {
+
+    const app = fastify({ logger: config.debug });
+    const port = process.env.PORT || config.port || 8080;
+
+    try {
+        app.register((instance, options, next) => {
+            // Routes
+            siteRoutes(instance);
+            next();
+        });
+
+        app.listen({ port: port }, (err) => {
+            if (err) {
+                app.log.error(err)
+                process.exit(1)
+            }
+        })
+
+        console.log(`\n// ${packageData.name} v.${packageData.version}\nGitHub Repository: ${packageData.homepage}\nCreated By: ${packageData.author}`);
+        console.log(`API is listening to the port ${port}`);
+    } catch (error) {
+        app.log.error(`Unable to start the server:\n${error}`);
+    }
+};
+
+buildApp();
---
+++ routes/index.js
@@ -0,0 +1,37 @@
+import fetch from 'node-fetch';
+import cheerio from 'cheerio';
+import moment from "moment";
+import stringify from "json-stringify-safe";
+
+export default function applicationSiteRoutes(app) {
+
+    app.get('/devotion/get', async function (req, res) {
+        try {
+            const response = await fetch('https://www.intouchaustralia.org/read/daily-devotions');
+            const html = await response.text();
+            const $ = cheerio.load(html);
+
+            const devotionTitle = $('h1').first().text().trim();
+            const date = moment(new Date(), 'dddd, MMMM D, YYYY').toDate();
+            const devotionContent = $('article.js-scripturize').find('p');
+
+            const contentArray = devotionContent.map((i, el) => $(el).text().trim()).get();
+            const bibleInOneYear = contentArray.splice(-1, 1)[0];
+            const thinkAboutIt = contentArray.splice(-1, 1)[0];
+
+            const devotion = {
+                title: devotionTitle,
+                date: date,
+                content: contentArray,
+                thinkAboutIt: thinkAboutIt.replace(/^•\s+/i, ''),
+                bibleInOneYear: bibleInOneYear.replace(/^Bible in One Year:\s+/i, '')
+            };
+
+            return res.send(devotion);
+
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    });
+}
